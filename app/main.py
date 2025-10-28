8441c8a6a5cc2cc66c3f043fbd135f1b9834a4fb
Emmanuel <emmanuelchiedueke01@gmail.com>
2025-10-28 13:23:18 +0100

    refactored for docker deployment

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from app.routes import tasks_sql, calculations_sql, market_sql
 from app.routes import task_auth, market_auth, Calculation_auth
 from fastapi import FastAPI
-
+import uvicorn
+import os
 
 app = FastAPI(title="Three Dimensions", version="1.0")
 
@@ -18,3 +19,8 @@ def home_page():
     return {
         "message": "Welcome to Three Dimensions API. Visit /docs to explore the endpoints."
     }
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
